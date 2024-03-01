a1518afba18a20cbf4295c71a1b82a10ae0cc2b1
WBL_Intern <you@example.com>
2024-03-01 22:45:34 +0530

    Ui  of cwpiReport

--- src/components/Atom/DatePIcker.jsx
+++ src/components/Atom/DatePIcker.jsx
@@ -5,7 +5,7 @@ const DatePicker = () => {
     <div className="relative max-w-sm">
       <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
         <svg
-          className="w-4 h-4 text-gray-500 dark:text-gray-400"
+          className="w-4 h-4 text-[#7575bf] border-[#7a7ab6]"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -16,7 +16,7 @@ const DatePicker = () => {
       </div>
       <input
         type="text"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border text-[#7575bf] border-[#7a7ab6]  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 placeholder-[#7575bf] "
         placeholder="Select date"
       />
     </div>
--- src/components/Atom/Pagination.jsx
+++ src/components/Atom/Pagination.jsx
@@ -8,7 +8,7 @@ const items = [
 
 export default function Example() {
   return (
-    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+    <div className="flex items-center border-t justify-between pt-10 border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 justify-between sm:hidden">
         <a
           href="#"
@@ -31,7 +31,7 @@ export default function Example() {
           </p>
         </div>
         <div>
-          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+          <nav className="isolate inline-flex -space-x-px  rounded-md shadow-sm" aria-label="Pagination">
             <a
               href="#"
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
@@ -39,7 +39,6 @@ export default function Example() {
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </a>
-            {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
             <a
               href="#"
               aria-current="page"
