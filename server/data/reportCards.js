16446e2f366c70e7d5db2a987fb67ea2e4c37872
developerShreya <you@example.com>
2024-03-08 12:54:55 +0300

    created  Report card  api
     dashboard  Report card  cards api

---
+++ server/data/reportCards.js
@@ -0,0 +1,30 @@
+const reportCardDetails = [{
+    "reportDetails":"Generate POP for Employee"
+  },
+  {
+    "reportDetails":"Report card of CWPI"
+  },
+  {
+    "reportDetails":"Delta Change"
+  },
+  {
+    "reportDetails":"CWPI Ranking Bottom 3 Performers"
+  },
+  {
+  
+    "reportDetails":"Delta Change for CWPI Ranking Bottom 3 Performers"
+  },
+  {
+  
+    "reportDetails":"CWPI Ranking Top 3 Performers"
+  },
+  {
+  
+    "reportDetails":"Delta Change for CWPI Ranking Top 3 Performers"
+  }
+]
+   
+  
+     
+  
+    module.exports = {reportCardDetails}
--- server/routes/cwpi/index.js
+++ server/routes/cwpi/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const workAgenciesRoutes = require('./workAgenciesRoutes');
 const schemeReportRoutes = require('./schemeReportRoutes');
-
+const reportCardDetails = require('./reportCardRoutes');
 const router = express.Router();
 
 router.use('/workAgencies', workAgenciesRoutes);
 router.use('/schemeReport', schemeReportRoutes);
+router.use('/reportCard', reportCardDetails);
 module.exports = router;
