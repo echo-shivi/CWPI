cdcffd292b6e3d13dc436baa5011681aaea31fb3
developerShreya <you@example.com>
2024-03-08 14:37:50 +0300

    created pdf reports  api
     dashboard  pdf reports  cards api

---
+++ server/data/dashboard/pdfReport.js
@@ -0,0 +1,12 @@
+const pdfReportsDetails = [{
+    "reportDetails":"Generate Reports for schemes"
+  },
+  {
+    "reportDetails":"Generate POP for Employees"
+  }
+]
+   
+  
+     
+  
+    module.exports = {pdfReportsDetails}
