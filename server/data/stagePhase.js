b7a15d828a0cea0e268c3e6c443a15c1efaf48db
developerShreya <you@example.com>
2024-03-08 13:05:05 +0300

    created stage wise  api
     dashboard  stage wise  cards api

---
+++ server/data/stagePhase.js
@@ -0,0 +1,26 @@
+const StageWiseAnalyticsDetails = [{
+    "stagePhase": "P1"
+},
+{
+    "stagePhase": "P2"
+},
+{
+    "stagePhase": "P3"
+},
+{
+    "stagePhase": "P4"
+},
+{
+
+    "stagePhase": "P5"
+},
+{
+
+    "stagePhase": "P6"
+},
+{
+
+    "stagePhase": "P7"
+}
+]
+module.exports = { StageWiseAnalyticsDetails }
--- server/routes/cwpi/index.js
+++ server/routes/cwpi/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const workAgenciesRoutes = require('./workAgenciesRoutes');
 const schemeReportRoutes = require('./schemeReportRoutes');
 const reportCardDetails = require('./reportCardRoutes');
+const StageWiseDetails = require('./stageAnalyticsRoutes')
 const router = express.Router();
 
 router.use('/workAgencies', workAgenciesRoutes);
 router.use('/schemeReport', schemeReportRoutes);
 router.use('/reportCard', reportCardDetails);
+router.use('/stageWise', StageWiseDetails);
 module.exports = router;
