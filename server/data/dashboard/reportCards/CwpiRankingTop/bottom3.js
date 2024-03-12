e510cbd998b46b01b325cc98fc75157b135720dc
developerShreya <you@example.com>
2024-03-12 14:20:06 +0300

    minor change

--- server/data/dashboard/reportCards/CwpiRankingTop/bottom3.js
+++ server/data/dashboard/reportCards/CwpiRankingTop/bottom3.js
@@ -1,12 +1,12 @@
 const bottom3 = [
     { "id": 1, "departmentName": 'Minor Water Resource Department(MWRD) ', "averageScore": 0, "Stage": "P3" },
-    { "id": 2, "departmentName": 'Bihar Police Building Construction Corporation(BPBCC)', "cwpiScore": 0, "Stage": "P3" },
+    { "id": 2, "departmentName": 'Bihar Police Building Construction Corporation(BPBCC)', "averageScore": 0, "Stage": "P3" },
 
-    { "id": 3, "departmentName": 'Infrastructure Development Authority(IDA)', "cwpiScore": 0, "Stage": "P3"},
-    { "id": 4, "departmentName": 'Bihar State Tourism Developemnt Corporation(BSTDC)', "cwpiScore": 0, "Stage": "P3" },
+    { "id": 3, "departmentName": 'Infrastructure Development Authority(IDA)', "averageScore": 0, "Stage": "P3"},
+    { "id": 4, "departmentName": 'Bihar State Tourism Developemnt Corporation(BSTDC)', "averageScore": 0, "Stage": "P3" },
 
-    { "id": 5, "departmentName": 'Bihar Medical Services & Infrastructure Corporation Ltd.(BMSICL)', "cwpiScore": 0, "Stage": "P3" },
+    { "id": 5, "departmentName": 'Bihar Medical Services & Infrastructure Corporation Ltd.(BMSICL)', "averageScore": 0, "Stage": "P3" },
 
-    { "id": 6, "departmentName": 'Bihar Urban Infrastructure Development Corporation (BUIDCO)', "cwpiScore": 0, "Stage": "P3" },
+    { "id": 6, "departmentName": 'Bihar Urban Infrastructure Development Corporation (BUIDCO)', "averageScore": 0, "Stage": "P3" },
 ]
 module.exports = { bottom3 }
