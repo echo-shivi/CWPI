1eb886bdd35bd931a32d4b3ee361eacd8df41a15
developerShreya <you@example.com>
2024-03-12 11:13:38 +0300

    Revert " api call and create of delta change"
    This reverts commit bf65db81b95423d9756f2bb9f1585a86cd1ab70b.

--- server/routes/dashboard/reportCard/reportDetails/index.js
+++ server/routes/dashboard/reportCard/reportDetails/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const cwpiReport = require('./cwpiReport');
 
-const deltaChange = require('./deltaChange');
 
 
 const router = express.Router();
 
 router.use('/cwpiReport', cwpiReport);
-router.use('/deltaChange', deltaChange);
 
 
 module.exports = router;
