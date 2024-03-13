758ad9720ec2668af092de1be8354c2d6b5e743f
developerShreya <you@example.com>
2024-03-13 14:51:33 +0300

    change heading size

--- client/src/components/Common/Navbar.jsx
+++ client/src/components/Common/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className='bg-blue-400 text-gray-100 px-4 h-28 w-[100%]'>
       <div className='flex justify-between px-4 '>
-        <h1 className='mt-12 text-2xl md:text-2xl underline'>
+        <h1 className='mt-12 text-xl md:text-2xl underline'>
           CIVIL WORKS PENDENCY INDEX
         </h1>
         <img src={BSLogo} alt='Logo' className='w-24 h-24 mt-3' />
--- client/src/components/Widgets/Dashboard/Dashboard.jsx
+++ client/src/components/Widgets/Dashboard/Dashboard.jsx
@@ -25,7 +25,7 @@ const Dashboard = () => {
 
   return (
     <div className='mx-0 flex flex-col w-full pt-[36px]'>
-      <h1 className=' md:text-4xl font-normal text-gray-700 text-lg sm:text-xl'>Welcome TO Super Admin Dashboard</h1>
+      <h1 className='text-xl md:text-2xl font-normal text-gray-700  '>Welcome TO Super Admin Dashboard</h1>
       <div className='pt-8 justify-center items-center'>
         {DashboardData.map((item) => (
          <DashboardLayout
