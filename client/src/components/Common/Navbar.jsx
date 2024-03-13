68a559ad96739aae8c0fc6c26c0f65d9f435dba2
developerShreya <you@example.com>
2024-03-13 12:02:26 +0300

    add logo in navbar

--- client/src/components/Common/Navbar.jsx
+++ client/src/components/Common/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import BSLogo from '../../assets/bihar_sarkar_logo.png';
 
 function Navbar() {
   return (
-    <div className='bg-blue-400 text-gray-100 px-4 h-24 w-[100%]'>
-      navbar
+    <div className='bg-blue-400 text-gray-100 px-4 h-28 w-[100%]'>
+      <div className='flex justify-between px-4 '>
+        <h1 className='mt-12 text-2xl md:text-2xl underline'>
+          CIVIL WORKS PENDENCY INDEX
+        </h1>
+        <img src={BSLogo} alt='Logo' className='w-24 h-24 mt-3' />
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
