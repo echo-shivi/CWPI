a6b6273cd0a3183ef9331ec07455979273c3b88d
developerShreya <you@example.com>
2024-03-13 14:48:09 +0300

    change pagination cursor

--- client/src/components/Atom/Pagination.jsx
+++ client/src/components/Atom/Pagination.jsx
@@ -5,7 +5,7 @@ const Pagination = ({ currentPage, totalPages, firstIndex, onPageChange, lastInd
     <nav className=" flex justify-between pt-10" aria-label="Pagination">
       <p className='text-base font-medium'>Showing {firstIndex} to {lastIndex} of {totalEntries} </p>
       <ul className='flex'>
-        <li onClick={prevPage} className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-not-allowed rounded-l-md">
+        <li onClick={prevPage} className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-pointer rounded-l-md">
           <span className="sr-only">Previous</span>
           <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M8.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 10H16a1 1 0 010 2H5.414l3.293 3.293a1 1 0 010 1.414z" clipRule="evenodd" />
@@ -13,14 +13,14 @@ const Pagination = ({ currentPage, totalPages, firstIndex, onPageChange, lastInd
         </li>
         {
           pages.map((page, index) => (
-            <li key={index} onClick={() => onPageChange(page)} className={`${currentPage === totalPages ? 'active' : ''} relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700  border  ${currentPage === page ? 'bg-blue-300 border-blue-300' : 'bg-white border-gray-300'}`}>
+            <li key={index} onClick={() => onPageChange(page)} className={`${currentPage === totalPages ? 'active' : ''} relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 cursor-pointer border  ${currentPage === page ? 'bg-blue-300 border-blue-300' : 'bg-white border-gray-300'}`}>
               {page}
 
             </li>
           ))
         }
 
-        <li onClick={nextPage} className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-not-allowed rounded-r-md">
+        <li onClick={nextPage} className="relative cursor-pointer inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md">
           <span className="sr-only">Next</span>
           <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M11.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 10H4a1 1 0 010-2h10.586L11.293 6.707a1 1 0 010-1.414z" clipRule="evenodd" />
