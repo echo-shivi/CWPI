8e81f2300e2ef88451055af5e4e2732ca2d28570
developerShreya <you@example.com>
2024-03-18 16:59:53 +0300

    fix barchart in server.js

--- server/routes/dashboard/barchart.js
+++ server/routes/dashboard/barchart.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const router = express.Router();
-const BarCharts = require('../../data/dashboard/barchart');
+const BarCharts = require('../../data/dashboard/barChart');
 
 router.get('/details',
   (req, res) => {
