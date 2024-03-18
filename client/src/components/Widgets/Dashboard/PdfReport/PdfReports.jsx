b1e637d0300321fabf68334bc156f3a30f8ad733
developerShreya <you@example.com>
2024-03-18 17:22:19 +0300

    fix pdf-reports routes

--- client/src/components/Widgets/Dashboard/PdfReport/PdfReports.jsx
+++ client/src/components/Widgets/Dashboard/PdfReport/PdfReports.jsx
@@ -25,7 +25,7 @@ const PdfReports = () => {
       {cardsData.map((card, index) => (
         <Link
           key={index}
-          to={card.reportDetails === "Generate Reports for schemes" ? "/masterentry" : "/scheme-details"}
+          to={card.reportDetails === "Generate Reports for schemes" ? "/masterentry" : "/report-card/1"}
           className="flex justify-between p-4 lg:p-6 bg-blue-300 text-black border border-gray-200 rounded-lg shadow hover:bg-blue-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
         >
           <p className="text-sm md:text-base font-normal mb-2">
